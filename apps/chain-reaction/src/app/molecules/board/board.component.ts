7ec2ca6761777ad91dc6d163e3e98607855f5871
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-11-04 12:25:32 +0530

    Made the board interactable for 1 player

--- apps/chain-reaction/src/app/molecules/board/board.component.ts
+++ apps/chain-reaction/src/app/molecules/board/board.component.ts
@@ -1,3 +1,4 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,14 +10,61 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   dimensions = [16, 16];
   currentPlayer = 0;
-  board = new Array(this.dimensions[0]).fill(0).map((a) =>
-    new Array(this.dimensions[1]).fill(0).map(() => ({
-      color: 'yellow',
-      count: Math.round(Math.random() * 10000) % 4,
-    }))
+  board: IBlock[][] = new Array(this.dimensions[0]).fill(0).map((a, colNo) =>
+    new Array(this.dimensions[1]).fill(0).map((b, rowNo) => {
+      let maxCount = 3;
+      if (rowNo === 0 || rowNo === this.dimensions[1] - 1) {
+        maxCount--;
+      }
+      if (colNo === 0 || colNo === this.dimensions[0] - 1) {
+        maxCount--;
+      }
+      return {
+        color: 'yellow',
+        count: 0,
+        maxCount,
+        rowNo,
+        colNo,
+      };
+    })
   );
 
   constructor() {}
 
+  addCount(rowNo: number, colNo: number): void {
+    const currentItem = this.board[colNo][rowNo];
+    if (currentItem.count < currentItem.maxCount) {
+      currentItem.count++;
+    } else {
+      currentItem.count = 0;
+      if (this.board[colNo + 1] && this.board[colNo + 1][rowNo]) {
+        this.addCount(rowNo, colNo + 1);
+      }
+      if (this.board[colNo - 1] && this.board[colNo - 1][rowNo]) {
+        this.addCount(rowNo, colNo - 1);
+      }
+      if (this.board[colNo][rowNo - 1]) {
+        this.addCount(rowNo - 1, colNo);
+      }
+      if (this.board[colNo][rowNo + 1]) {
+        this.addCount(rowNo + 1, colNo);
+      }
+    }
+  }
+
+  playMove(playerNo: number, block: IBlock): void {
+    this.addCount(block.rowNo, block.colNo);
+
+    return;
+  }
+
   ngOnInit(): void {}
+}
+
+export interface IBlock {
+  color: string;
+  count: number;
+  maxCount: number;
+  rowNo: number;
+  colNo: number;
 }
