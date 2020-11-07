b6dfb4a94780ff14565cc2ac0c0bb57381395b4b
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-11-07 11:32:52 +0530

    Added new page structure

--- apps/chain-reaction/src/app/app-routing.module.ts
+++ apps/chain-reaction/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GameComponent } from './game/game.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- apps/chain-reaction/src/app/app.module.ts
+++ apps/chain-reaction/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GameComponent } from './game/game.component';
-import { HomeComponent } from './home/home.component';
+import { GameComponent } from './pages/game/game.component';
+import { HomeComponent } from './pages/home/home.component';
 import { BoardComponent } from './molecules/board/board.component';
 import { BlockComponent } from './atoms/block/block.component';
 
